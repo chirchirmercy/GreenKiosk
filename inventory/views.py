67bd0c58839a2a4da69b692ea54cacbfa016dfee
chirchirmercy <cmercycheptoo@gmail.com>
2023-07-20 07:34:50 +0300

    changes

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from.forms import ProductUploadForm
 
 # Create your views here.
+
+
+# Create your views here.def product_upload_view(request):
+def product_upload_view(request):
+    form =ProductUploadForm()
+    return render(request,"inventory/product_upload.html",{"form":form})
