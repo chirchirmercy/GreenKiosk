44b1859482d094396058bdb4dfb47659f1f83422
chirchirmercy <cmercycheptoo@gmail.com>
2023-08-11 12:50:13 +0300

    changes

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from.views import product_upload_view
+from.views import product_upload_view,products_list
+
 urlpatterns=[
     path("products/upload/",product_upload_view,name="product_uploadview"),
+    path("products/list/",products_list,name="products_list"),
 ]
+
+
+
+
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from.forms import ProductUploadForm
+from .models import Product
 
 # Create your views here.
 
@@ -8,3 +9,12 @@ from.forms import ProductUploadForm
 def product_upload_view(request):
     form =ProductUploadForm()
     return render(request,"inventory/product_upload.html",{"form":form})
+
+def products_list(request):
+    projects=Product.objects.all()
+    return render (request,"inventory/products_list.html",{"products":Product})
+    
+
+def product_detail_view(request,id):
+    product=product.objects.get(id=id)
+    return render(request,"inventory/product_details.html", {"product":product})
