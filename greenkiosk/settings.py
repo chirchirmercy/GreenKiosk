68155da18fcfdbfc1a401e2dbc6360022637e4a7
chirchirmercy <cmercycheptoo@gmail.com>
2023-08-14 06:49:55 +0300

    changes

--- greenkiosk/settings.py
+++ greenkiosk/settings.py
@@ -91,7 +91,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
